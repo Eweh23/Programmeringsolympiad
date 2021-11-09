f8020f0783c51958a496f379b975dde926dd3439
Eweh23 <92167640+Eweh23@users.noreply.github.com>
2021-11-09 16:21:14 +0100

    Add files via upload

---
+++ Uppgift 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antal med grönt kort?");
+            int M =int.Parse(Console.ReadLine());
+            Console.WriteLine("Antal utan grönt kort?");
+            int N =int.Parse(Console.ReadLine());
+            int Tid=10*(N/M+ 2*M/M); 
+            Console.WriteLine(Tid);
+        }
+    }
+}
